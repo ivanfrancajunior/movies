4842f6429aff57a2f4e8e975bfe0e7c246f10d12
Ivan <contato.ivanfrancajr@gmail.com>
2023-11-01 22:07:27 -0300

    adiciona navbar + assets

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
 import { options } from "../utils";
 import { useNavigate } from "react-router-dom";
+import { RiMenuLine, RiSearchLine } from "react-icons/ri";
+import logoIcon from "../assets/icons8-camera-40.png";
 export const Navbar = () => {
   const [query, setQuery] = useState("");
+
   const navegate = useNavigate();
 
   const getData = async () => {
@@ -27,13 +30,13 @@ export const Navbar = () => {
     navegate(`/search/${query}`);
   };
   return (
-    <nav className="bg-[#080808] h-24 p-4 flex items-center justify-start gap-20 text-[#d8d8d8] font-bold">
-      <h1>Logo muito elegante </h1>
+    <nav className="bg-[#080808] h-24 px-20  flex items-center justify-between gap-20  text-[#d8d8d8] font-bold  w-full">
+      <h1 className="min-w-24  flex items-center justify-center">
+        <img src={logoIcon} alt="" className="h-10 w-10"/>
+      </h1>
       <form
-        action=""
-        className="flex items-center justify-between gap-5 ml-20"
+        className="hidden md:flex  items-center justify-center max-w-[600px] gap-2 p-2"
         onSubmit={handleSubmit}
-        placeholder="Search"
       >
         <input
           type="text"
@@ -41,11 +44,16 @@ export const Navbar = () => {
           value={query}
           onChange={(e) => setQuery(e.target.value)}
           placeholder="Search"
+          className="    md:w-[5580px] py-2 px-4 rounded-lg "
         />
-        <button type="submit" className="border">
-          Search
+        <button type="submit">
+          <RiSearchLine size={24} />
         </button>
       </form>
+      <div className="md:hidden w-1/6">
+        <RiMenuLine size={24} />
+      </div>
+      <div className=" hidden md:flex md:w-1/6"></div>
     </nav>
   );
 };
